87a844a9a389a4183aa52570e95bd3fe8f11c0c2
Stefan <stefan.jovanavich@gmail.com>
2021-09-14 12:17:12 +0200

    Dependency Update

--- app/src/main/java/com/example/to_docompose/navigation/destinations/ListComposable.kt
+++ app/src/main/java/com/example/to_docompose/navigation/destinations/ListComposable.kt
@@ -5,13 +5,13 @@ import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.LaunchedEffect
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
-import com.google.accompanist.navigation.animation.composable
 import androidx.navigation.compose.navArgument
 import com.example.to_docompose.ui.screens.list.ListScreen
 import com.example.to_docompose.ui.viewmodels.SharedViewModel
 import com.example.to_docompose.util.Constants.LIST_ARGUMENT_KEY
 import com.example.to_docompose.util.Constants.LIST_SCREEN
 import com.example.to_docompose.util.toAction
+import com.google.accompanist.navigation.animation.composable
 
 @ExperimentalAnimationApi
 @ExperimentalMaterialApi
